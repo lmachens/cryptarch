0e53f00a5cba395700cefcad5e19439462510e42
lmachens <leon@machens.koeln>
2021-05-11 14:39:10 +0200

    :sparkles: Ask for main password

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,27 @@
-import fs from 'fs';
+import { askForMainPassword } from './utils/questions';
+import { isMainPasswordValid } from './utils/validation';
 
-console.log(fs.readFileSync('./README.md', 'utf-8'));
+// function start() {
+const start = async () => {
+  /* Solution with while */
+  let mainPassword = await askForMainPassword();
+  while (!isMainPasswordValid(mainPassword)) {
+    console.log('Is invalid');
+    mainPassword = await askForMainPassword();
+  }
+  console.log('Is valid');
+
+  /* Solution with recursion */
+  //   const mainPassword = await askForMainPassword();
+  //   if (!isMainPasswordValid(mainPassword)) {
+  //     console.log('Is invalid');
+  //     start(); // Recursion
+  //   } else {
+  //     console.log('Is valid');
+  //   }
+
+  /* ToDo */
+  // askForCommand();
+};
+
+start();
