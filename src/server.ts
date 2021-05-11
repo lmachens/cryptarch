922da3009f27f982a1d681f704b433a278adf46d
phlgr <philipp@gartz.dev>
2021-05-11 16:30:27 +0200

    ✨ Implement  chooseCommand function

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { askForMainPassword } from './utils/questions';
+import { askForMainPassword, chooseCommand } from './utils/questions';
 import { isMainPasswordValid } from './utils/validation';
 
 // function start() {
@@ -10,6 +10,17 @@ const start = async () => {
     mainPassword = await askForMainPassword();
   }
   console.log('Is valid');
+
+  const command = await chooseCommand();
+
+  switch (command) {
+    case 'list':
+      console.log('List Case');
+      break;
+    case 'add':
+      console.log('Add Case');
+      break;
+  }
 
   /* Solution with recursion */
   //   const mainPassword = await askForMainPassword();
--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,4 +1,5 @@
 import inquirer from 'inquirer';
+import { Command } from '../types';
 
 // export function askForMainPassword(): Promise<string> {
 export const askForMainPassword = (): Promise<string> => {
@@ -11,4 +12,17 @@ export const askForMainPassword = (): Promise<string> => {
       },
     ])
     .then((answers) => answers.mainPassword);
+};
+
+export const chooseCommand = async (): Promise<Command> => {
+  const answers = await inquirer.prompt<{ command: Command }>({
+    type: 'list',
+    name: 'command',
+    message: 'What do you want to do?',
+    choices: [
+      { name: 'List all credentials', value: 'list' },
+      { name: 'Add new credential', value: 'add' },
+    ],
+  });
+  return answers.command;
 };
