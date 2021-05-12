488f33619400564de1a53d822ad226c1da2191e8
lmachens <leon@machens.koeln>
2021-05-12 16:21:44 +0200

    :lock: Verify that password hash is equal to master password hash

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,8 +1,10 @@
 import fs from 'fs/promises';
+import sha256 from 'crypto-js/sha256';
 
 export const isMainPasswordValid = async (
   plaintextPassword: string
 ): Promise<boolean> => {
-  const passwordHash = await fs.readFile('./.password', 'utf-8');
-  return plaintextPassword === passwordHash;
+  const origPasswordHash = await fs.readFile('./.password', 'utf-8');
+  const passwordHash = sha256(plaintextPassword).toString();
+  return passwordHash === origPasswordHash;
 };
