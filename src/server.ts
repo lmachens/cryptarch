a61b66e1dc02a9bedb8e7c74b08972ff7945cb3a
lmachens <leon@machens.koeln>
2021-05-19 11:02:48 +0200

    ➕ Add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-console.log('Server is running');
+import express from 'express';
 
-export {};
+const app = express();
+const port = 5000;
+
+app.get('/api/credentials', (_request, response) => {
+  response.send('All credentials requested');
+});
+
+app.post('/api/credentials', (_request, response) => {
+  response.send('Add new credential');
+});
+
+app.listen(port, () => {
+  console.log(`Cryptarch listening at http://localhost:${port}`);
+});
