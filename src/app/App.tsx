0e3f58fe12e2f8def2e76c9493bfee944e781543
lmachens <leon@machens.koeln>
2021-05-21 10:21:08 +0200

    ✨ Add sample App

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App(): JSX.Element {
+  return (
+    <div className={styles.App}>
+      <h1>cryptarch</h1>
+    </div>
+  );
+}
+
+export default App;
